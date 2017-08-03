40503315f47fb844a478f6f625730857e8b39a00
Alexey Neybauer <xardasspam@gmail.com>
2017-08-03 10:33:21 +0300

    Поправлена индентация

--- server.js
+++ server.js
@@ -14,25 +14,28 @@ mongoose.Promise = global.Promise
 mongoose.connect(db.url, {useMongoClient: true})
 
 app.use((req, res, next) => {
-  var oneof = false;
-      if(req.headers.origin) {
-          res.header('Access-Control-Allow-Origin', req.headers.origin);
-          oneof = true;
-      }
-      if(req.headers['access-control-request-method']) {
-          res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
-          oneof = true;
-      }
-      if(req.headers['access-control-request-headers']) {
-          res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
-          oneof = true;
-      }
-      if(oneof) {
-        console.log(2)
-          res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
-      }
+  var oneof = false
 
-      next();
+  if (req.headers.origin) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin)
+    oneof = true
+  }
+
+  if (req.headers['access-control-request-method']) {
+    res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method'])
+    oneof = true
+  }
+
+  if (req.headers['access-control-request-headers']) {
+    res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers'])
+    oneof = true
+  }
+
+  if (oneof) {
+    res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365)
+  }
+
+  next()
 })
 
 app.use((req, res, next) => {
